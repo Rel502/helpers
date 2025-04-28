f8c8c1fa486d321fc12cb9fd61f8a99eea160692
Michael Zeitler <zeimic@maincubes.com>
2025-04-28 11:42:12 +0200

    ADDED: includeHTML function

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ function init() {
     console.log("läuft...");
 }
 
+
+/**
+ * This function is used to load external HTML files into elements that have the 'w3-include-html'
+ * attribute. It fetches the specified files and replaces the innerHTML of the elements with the
+ * fetched content.
+ */
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+
 /**
  * This function is used to toggle the visibility of a button
  * 
