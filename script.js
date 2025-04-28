58e1e97692e42c922febbbb8de58e00b45047578
Michael Zeitler <zeimic@maincubes.com>
2025-04-28 10:15:31 +0200

    ADDED: togglePopup(popupId, param)

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function init() {
  * This function is used to toggle the visibility of a button
  * 
  * @param {string} id - the id of the button that shall be toggled
- * @param {string} state - The state that the button should assume
+ * @param {string} state - The state that the button should assume (hide or show)
  */
 function toggleButton(id, state) {
     let button = document.getElementById(id);
@@ -15,5 +15,20 @@ function toggleButton(id, state) {
         button.classList.add('d-none');
     } else if (state == 'show') {
         button.classList.remove('d-none');
+    }
+}
+
+
+/**
+ * This function is used to show or hide a popup
+ * 
+ * @param {string} popupId - the ID of the popup element
+ * @param {string} param - the state that the popup should assume (hide or show)
+ */
+function togglePopup(popupId, param) {
+    if (param == 'show') {
+        document.getElementById(popupId).classList.remove('d-none');
+    } else {
+        document.getElementById(popupId).classList.add('d-none');
     }
 }
